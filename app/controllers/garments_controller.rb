d7e479fcbb17a1d95f05395247ae8860ec2b2498
Darin Ma <darinmma@gmail.com>
2016-10-25 11:12:02 -0700

    adds index view

--- app/controllers/garments_controller.rb
+++ app/controllers/garments_controller.rb
@@ -1,13 +1,16 @@
 class GarmentsController < ApplicationController
   def index
+    @garments = Garment.all
   end
 
   def show
+    @garment = Garment.find(params[:id])
+  end
+
+  def new
   end
 
   def edit
   end
 
-  def new
-  end
 end
